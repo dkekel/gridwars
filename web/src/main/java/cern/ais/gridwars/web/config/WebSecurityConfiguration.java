4c354e9b326445a131f04e63f91ef34a17c88476
Benjamin Wolff <benjamin.wolff@cern.ch>
2018-03-21 18:55:14 +0100

    First steps for implementing web security

--- web/src/main/java/cern/ais/gridwars/web/config/WebSecurityConfiguration.java
+++ web/src/main/java/cern/ais/gridwars/web/config/WebSecurityConfiguration.java
@@ -1,8 +1,13 @@
 package cern.ais.gridwars.web.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 
 @Configuration
@@ -10,7 +15,33 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
         http.authorizeRequests()
-            .anyRequest().permitAll();
+//            .anyRequest().authenticated()
+            .anyRequest().permitAll()
+//            .antMatchers("/admin/**").hasAuthority("ADMIN")
+            .and()
+                .formLogin().permitAll()
+            .and()
+                .logout().logoutSuccessUrl("/").permitAll();
+        // @formatter:on
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("bla")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("blabla")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
--- web/src/main/java/cern/ais/gridwars/web/controller/IndexController.java
+++ web/src/main/java/cern/ais/gridwars/web/controller/IndexController.java
@@ -1,16 +1,21 @@
 package cern.ais.gridwars.web.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+import java.util.Optional;
 
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("name", "Ben");
+    public String index(Model model, @AuthenticationPrincipal Principal currentUser) {
+        String username = Optional.ofNullable(currentUser).map(Principal::getName).orElse("<anonymous>");
+        model.addAttribute("user", username);
         return "index";
     }
 }
