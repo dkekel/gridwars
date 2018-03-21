1f40869b49c62e2293389a7a547ba2285b0092d5
Benjamin Wolff <benjamin.wolff@cern.ch>
2018-03-21 18:14:10 +0100

    Disable default Spring web security constraints for development

--- web/src/main/java/cern/ais/gridwars/web/controller/IndexController.java
+++ web/src/main/java/cern/ais/gridwars/web/controller/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping(path = { "", "/" })
+    @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("name", "Ben");
         return "index";
