fdbc2bc5fc0ad900485088fadd9dd313607623d9
Benjamin Wolff <benjamin.wolff@cern.ch>
2018-04-05 23:53:54 +0200

    Reconfigure handling of static resources and enable caching and compression

--- web/src/main/java/cern/ais/gridwars/web/config/WebSecurityConfiguration.java
+++ web/src/main/java/cern/ais/gridwars/web/config/WebSecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -39,6 +40,12 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         // TODO disable csrf for now, as it breaks i.e. H2 console
         http.csrf().disable();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // Disable security chain for static resources
+        web.ignoring().antMatchers("/static/**");
     }
 
     @Override
