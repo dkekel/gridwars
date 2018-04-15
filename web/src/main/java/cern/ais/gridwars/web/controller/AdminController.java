865706b22f8e04ae3bfbca9423d498708e73b06a
Benjamin Wolff <benjamin.wolff@cern.ch>
2018-04-15 17:37:39 +0200

    Improve logging in admin controller

--- web/src/main/java/cern/ais/gridwars/web/controller/AdminController.java
+++ web/src/main/java/cern/ais/gridwars/web/controller/AdminController.java
@@ -1,7 +1,11 @@
 package cern.ais.gridwars.web.controller;
 
+import cern.ais.gridwars.web.domain.User;
 import cern.ais.gridwars.web.service.MatchWorkerService;
 import cern.ais.gridwars.web.util.ModelAndViewBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +20,7 @@ import java.util.Objects;
 @RequestMapping("/admin")
 public class AdminController {
 
+    private final Logger LOG = LoggerFactory.getLogger(getClass());
     private final MatchWorkerService matchWorkerService;
 
     public AdminController(MatchWorkerService matchWorkerService) {
@@ -32,23 +37,30 @@ public class AdminController {
     }
 
     @PostMapping(path = "/worker", params = { "action=start" })
-    public ModelAndView startWorkers(RedirectAttributes redirectAttributes) {
+    public ModelAndView startWorkers(RedirectAttributes redirectAttributes, @AuthenticationPrincipal User user) {
+        logAction("START", user);
         matchWorkerService.startAllMatchWorkers();
         redirectAttributes.addFlashAttribute("started", true);
         return ModelAndViewBuilder.forRedirect("/admin/worker").toModelAndView();
     }
 
     @PostMapping(path = "/worker", params = { "action=wakeup" })
-    public ModelAndView wakeUpWorkers(RedirectAttributes redirectAttributes) {
+    public ModelAndView wakeUpWorkers(RedirectAttributes redirectAttributes, @AuthenticationPrincipal User user) {
+        logAction("WAKE UP", user);
         matchWorkerService.wakeUpAllMatchWorkers();
         redirectAttributes.addFlashAttribute("wokenup", true);
         return ModelAndViewBuilder.forRedirect("/admin/worker").toModelAndView();
     }
 
     @PostMapping(path = "/worker", params = { "action=stop" })
-    public ModelAndView stopWorkers(RedirectAttributes redirectAttributes) {
+    public ModelAndView stopWorkers(RedirectAttributes redirectAttributes, @AuthenticationPrincipal User user) {
+        logAction("STOP", user);
         matchWorkerService.stopAllMatchWorkers();
         redirectAttributes.addFlashAttribute("stopped", true);
         return ModelAndViewBuilder.forRedirect("/admin/worker").toModelAndView();
+    }
+
+    private void logAction(String action, User user) {
+        LOG.info("Received worker {} request from: {}", action, user.getUsername());
     }
 }
