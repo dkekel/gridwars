674cf10eeef4a3057e6ce156136ce86457a376fe
Benjamin Wolff <benjamin.wolff@cern.ch>
2018-04-22 20:46:27 +0300

    Add some helper shell scripts

---
+++ web/scripts/backup-gridwars-db.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+cp deployed/work/db/gridwars.mv.db "backup/db/gridwars.mv.db.$(date +'%d-%m-%Y-%H%M%S')"
---
+++ web/scripts/backup-gridwars.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+tar -zcvf "backup/gridwars-$(date +'%d-%m-%Y-%H%M%S').tar.gz" deployed
