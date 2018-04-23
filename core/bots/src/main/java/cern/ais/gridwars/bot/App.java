b79444df91aa89313f8afa017c171139fa242c70
Benjamin Wolff <benjamin.wolff@cern.ch>
2018-04-23 09:29:30 +0300

    Prepare level 1 admin bot

--- core/bots/src/main/java/cern/ais/gridwars/bot/App.java
+++ core/bots/src/main/java/cern/ais/gridwars/bot/App.java
@@ -1,15 +1,23 @@
 package cern.ais.gridwars.bot;
 
 import cern.ais.gridwars.Emulator;
+import cern.ais.gridwars.api.bot.PlayerBot;
 
 import java.io.FileNotFoundException;
 
 public class App {
 	public static void main(String[] args) throws FileNotFoundException {
+//	    PlayerBot bot1 = new ExpandBot();
+
+//		Main bot1 = new Main();
+//		bot1.setBot(new FastExpandBot(bot1));
+
 		Main bot1 = new Main();
-		bot1.setBot(new FastExpandBot(bot1));
-		Main bot2 = new Main();
-		bot2.setBot(new SplitBot(bot2));
+        bot1.setBot(new SplitBot(bot1));
+
+//        PlayerBot bot2 = new ExpandBot();
+
+        PlayerBot bot2 = new RocketBot();
 
 		Emulator.playMatch(bot1, bot2);
 	}
